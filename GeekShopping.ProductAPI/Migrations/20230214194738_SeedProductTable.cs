b36a89c09b45747ef15926b650209c1116f10bc2
Hugo Lima <limabrot879@gmail.com>
2023-02-14 17:20:15 -0300

    feat: add SeedProductTable migration

---
+++ GeekShopping.ProductAPI/Migrations/20230214194738_SeedProductTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "created_at", "description", "image_url", "name", "price", "updated_at" },
+                values: new object[,]
+                {
+                    { 1L, "Hobby", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Carta rara que paralisou leilao!", "https://s2.glbimg.com/qnz29OQJebXxDQo-slkyH68WWBA=/0x0:444x643/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/A/e/dyDPEFST6Ezs8CqBTnQg/dragao.jpg", "Carta Yu-Gi-Oh Rara", 22000m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2L, "Escolar", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Caneta azul, azul canetaaa...", "https://img.kalunga.com.br/fotosdeprodutos/176072z_2.jpg", "Caneta esferográfica azul", 2m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3L, "Escolar", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Aproveite essa volta às aulas com estilo!", "https://m.media-amazon.com/images/I/51Z8ZM4sj8L._AC_SX679_.jpg", "Caderno Tilibra Azul", 6.5m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Models/Context/PostgresContext.cs
+++ GeekShopping.ProductAPI/Models/Context/PostgresContext.cs
@@ -1,11 +1,71 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GeekShopping.ProductAPI.Models.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeekShopping.ProductAPI.Models.Context;
 
 public class PostgresContext : DbContext
 {
-  public PostgresContext() { }
-  public PostgresContext(DbContextOptions<PostgresContext> options) : base(options) { }
+	public PostgresContext()
+	{
+	}
 
-  public DbSet<Product> Products { get; set; } = default!;
+	public PostgresContext(DbContextOptions<PostgresContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Product> Products { get; set; } = default!;
+
+	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+	{
+		var entries = ChangeTracker
+			.Entries()
+			.Where(e => e.Entity is BaseEntity && (
+				e.State == EntityState.Added
+				|| e.State == EntityState.Modified));
+
+		foreach (var entry in entries)
+		{
+			((BaseEntity)entry.Entity).UpdatedAt = DateTime.Now;
+
+			if (entry.State == EntityState.Added)
+			{
+				((BaseEntity)entry.Entity).CreatedAt = DateTime.Now;
+			}
+		}
+
+		return base.SaveChangesAsync(cancellationToken);
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Product>().HasData(new Product
+		{
+			Id = 1,
+			Name = "Carta Yu-Gi-Oh Rara",
+			Price = new decimal(22000.0),
+			ImageUrl = "https://s2.glbimg.com/qnz29OQJebXxDQo-slkyH68WWBA=/0x0:444x643/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/A/e/dyDPEFST6Ezs8CqBTnQg/dragao.jpg",
+			Description = "Carta rara que paralisou leilao!",
+			CategoryName = "Hobby"
+		});
+
+		modelBuilder.Entity<Product>().HasData(new Product
+		{
+			Id = 2,
+			Name = "Caneta esferográfica azul",
+			Price = new decimal(2),
+			ImageUrl = "https://img.kalunga.com.br/fotosdeprodutos/176072z_2.jpg",
+			Description = "Caneta azul, azul canetaaa...",
+			CategoryName = "Escolar"
+		});
+
+		modelBuilder.Entity<Product>().HasData(new Product
+		{
+			Id = 3,
+			Name = "Caderno Tilibra Azul",
+			Price = new decimal(6.50),
+			ImageUrl = "https://m.media-amazon.com/images/I/51Z8ZM4sj8L._AC_SX679_.jpg",
+			Description = "Aproveite essa volta às aulas com estilo!",
+			CategoryName = "Escolar"
+		});
+	}
 }
