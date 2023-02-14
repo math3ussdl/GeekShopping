8ffc195332a4347d8f9afd77e73b8e8bb9bbb115
Hugo Lima <limabrot879@gmail.com>
2023-02-14 18:20:00 -0300

    refactor: add method to work with post and put methods

--- GeekShopping.Web/Utils/HttpClientExtensions.cs
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -1,9 +1,12 @@
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace GeekShopping.Web.Utils;
 
 public static class HttpClientExtensions
 {
+	private static readonly MediaTypeHeaderValue ContentType = new("application/json");
+
 	public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
 	{
 		if (!response.IsSuccessStatusCode)
@@ -20,5 +23,23 @@ public static class HttpClientExtensions
 			throw new ApplicationException("Something went wrong with response deserialization!");
 
 		return deserializedResponse;
+	}
+
+	public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+	{
+		var dataAsString = JsonSerializer.Serialize(data);
+		var content = new StringContent(dataAsString);
+		content.Headers.ContentType = ContentType;
+
+		return httpClient.PostAsync(url, content);
+	}
+
+	public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+	{
+		var dataAsString = JsonSerializer.Serialize(data);
+		var content = new StringContent(dataAsString);
+		content.Headers.ContentType = ContentType;
+
+		return httpClient.PutAsync(url, content);
 	}
 }
