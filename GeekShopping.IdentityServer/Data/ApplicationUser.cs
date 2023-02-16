555bb4dfc3b920604fb9b288f4e359fdc179cbda
Hugo Lima <limabrot879@gmail.com>
2023-02-16 18:10:32 -0300

    refactor: remove obsolete version of Duende IdentityServer

--- GeekShopping.IdentityServer/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace GeekShopping.IdentityServer.Data;
-
-public class ApplicationUser : IdentityUser
-{
-  private string FirstName { get; set; }
-  private string LastName { get; set; }
-}
--- GeekShopping.ProductAPI/Models/Product.cs
+++ GeekShopping.ProductAPI/Models/Product.cs
@@ -8,24 +8,17 @@ namespace GeekShopping.ProductAPI.Models;
 public class Product : BaseEntity
 {
 	[Column("name")]
-	[Required]
-	[StringLength(150)]
 	public string Name { get; set; } = default!;
 
 	[Column("price")]
-	[Required]
-	[Range(1, 10000)]
 	public decimal Price { get; set; }
 
 	[Column("description")]
-	[StringLength(500)]
 	public string Description { get; set; } = default!;
 
 	[Column("category_name")]
-	[StringLength(50)]
 	public string CategoryName { get; set; } = default!;
 
 	[Column("image_url")]
-	[StringLength(300)]
 	public string ImageUrl { get; set; } = default!;
 }
